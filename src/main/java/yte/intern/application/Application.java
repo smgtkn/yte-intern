29beb53739c28625268d041bd77f59d99eb6857e
smgtkn <smgtknn@gmail.com>
2020-08-04 01:50:03 +0300

    my changes

--- src/main/java/yte/intern/application/Application.java
+++ src/main/java/yte/intern/application/Application.java
@@ -11,9 +11,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(Application.class, args);
-		EtkinlikRepository etkinlikRepository=context.getBean(EtkinlikRepository.class);
-		Etkinlik etkinlik1=new Etkinlik(null,"simge","23/04/1999","21/09/2077");
-		etkinlikRepository.save(etkinlik1);
+	
 	}
 
 }
